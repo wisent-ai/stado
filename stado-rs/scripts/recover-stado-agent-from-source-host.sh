a9aadeb9a398967b1381cece8996ca55aa3eb955
lbartoszcze <lgb2127@columbia.edu>
2026-08-11 12:34:50 -0700

    Recover Stado agents from source

---
+++ stado-rs/scripts/recover-stado-agent-from-source-host.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eu
+
+repo="$HOME/.stado/build/stado"
+/bin/mkdir -p "$HOME/.stado/build" "$HOME/.stado/bin"
+if [ -d "$repo/.git" ]
+then
+  /usr/bin/git -C "$repo" fetch --prune origin main
+else
+  /usr/bin/git clone --filter=blob:none https://github.com/wisent-ai/stado.git "$repo"
+fi
+/usr/bin/git -C "$repo" checkout --detach origin/main
+cargo_bin=$(command -v cargo || true)
+[ -n "$cargo_bin" ] || cargo_bin="$HOME/.cargo/bin/cargo"
+[ -x "$cargo_bin" ]
+"$cargo_bin" build --release --manifest-path "$repo/stado-rs/Cargo.toml" --bin stado
+/usr/bin/install -m 0755 "$repo/stado-rs/target/release/stado" "$HOME/.stado/bin/stado.next"
+/bin/mv "$HOME/.stado/bin/stado.next" "$HOME/.stado/bin/stado"
+"$HOME/.stado/bin/stado" --version
