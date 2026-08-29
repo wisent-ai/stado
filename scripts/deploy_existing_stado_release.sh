d2b7b815e3cf69602e23b654d99df731639a9621
lbartoszcze <lgb2127@columbia.edu>
2026-08-28 20:58:43 -0700

    Preserve canonical API for self release

--- scripts/deploy_existing_stado_release.sh
+++ scripts/deploy_existing_stado_release.sh
@@ -62,7 +62,7 @@ while IFS=$'\t' read -r target platform; do
   ensure_host_archive "$platform" no
   "$stado_bin" host declare-version "$target" --binary stado --version "$version" --json
   if [ "$target" = "$self_target" ]; then
-    env -u STADO_API_URL -u STADO_API_TOKEN \
+    env -u STADO_API_TOKEN \
       "$stado_bin" host release "$target" --binary stado --version "$version" \
         --reinstall --json
   else
